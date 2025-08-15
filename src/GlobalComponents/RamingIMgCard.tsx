82b95ac594a0d1e2d4e0255ea87db0d08b2bb768
md jakria ahmod <jakariaahmodmd@gmail.com>
2025-08-15 20:55:29 +0600

    som

--- src/GlobalComponents/RamingIMgCard.tsx
+++ src/GlobalComponents/RamingIMgCard.tsx
@@ -7,8 +7,13 @@ const RamingIMgCard: React.FC<img> = ({ img }) => {
   return (
     <div>
       <div>
-        <div className="h-[526px] rounded overflow-hidden">
+        <div className="h-[526px] rounded overflow-hidden relative">
           <img src={img} alt="img" className="w-full h-full object-cover" />
+
+          <div className="bg-orange-700 absolute bottom-0 left-0 w-full flex justify-between px-[10px] py-4">
+            <span>Buy mint</span>
+            <span>20 sol</span>
+          </div>
         </div>
       </div>
     </div>
